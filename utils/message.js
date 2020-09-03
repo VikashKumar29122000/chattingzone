04285d94a606e5c0369e1fee40a8197502960ee1
VikashKumar29122000 <sgdcyu@gmail.com>
2020-09-03 11:32:59 +0530

    time fixed to the IST

--- utils/message.js
+++ utils/message.js
@@ -6,6 +6,6 @@ module.exports=function formatMessage(username,message){
     return {
         username:username,
         message:message,
-        time:(moment().utcOffset()== -0)?moment(Date.now()+28800000).format('h:mm a'):moment(Date.now()).format('h:mm a')
+        time:(moment().utcOffset()== -0)?moment(Date.now()+19800000).format('h:mm a'):moment(Date.now()).format('h:mm a')
     }
 }
